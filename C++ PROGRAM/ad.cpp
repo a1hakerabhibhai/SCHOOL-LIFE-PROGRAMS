40b97afb9cb446ef1d775adeb00a0c640aeccccd
a1hakerabhibhai <a1hakerabhibhai@gmail.com>
2024-09-30 20:48:30 +0545

    1st program

---
+++ C++ PROGRAM/ad.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num3,num7;
+
+    cout<<"Enter a number num3=" ;
+
+    cin>>num3;
+    cout<<"Enter a number num7=" ;
+
+    cin>>num7;
+    cout<<"the value of num3,num7 = " <<num3+num7;
+
+
+    return 0;
+
+
+
+
+
+
+}
---
+++ C++ PROGRAM/mul.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int b,c;
+    cout<<"press a number for multiply b=" ;
+    cin>>b;
+    cout<<"press a number for multiply c=" ;
+    cin>>c;
+    cout<<"The multiply value of b and c="<<b*c <<endl;
+    int d,e;
+    cout<<"press a number for substraction d=" ;
+    cin>>d;
+    cout<<"press a number for substraction e=" ;
+    cin>>e;
+    cout<<"The substraction value of d and e="<<d-e <<endl;
+int f,g;
+    cout<<"press a number for add f=" ;
+    cin>>f;
+    cout<<"press a number for add g=" ;
+    cin>>g;
+    cout<<"The add value of f and g="<<f+g <<endl;
+int h;
+float i;
+    cout<<"press a number for division h=" ;
+    cin>>h;
+    cout<<"press a number for division i=" ;
+    cin>>i;
+    cout<<"The division value of h and i="<<h/i <<endl;
+
+
+    return 0;
+
+}
