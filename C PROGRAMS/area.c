119b08467319090561d40530ac52a05a2124cbf6
a1hakerabhibhai <a1hakerabhibhai@gmail.com>
2024-10-21 20:53:21 +0545

    try to write a to calculate area of square

---
+++ C PROGRAMS/area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int l, b;
+    printf("Enter the length of square = ");
+    scanf("%d", &l);
+    printf("Enter the breadth of square = ");
+    scanf("%d", &b);
+    printf("The area of square is = %d", l*b);
+    return 0;
+}
